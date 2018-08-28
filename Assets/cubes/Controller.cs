02687f1718fee29b15cfef9dad4d7150b75932be
Cricetus <homyak1996@gmail.com>
2018-08-28 20:40:38 +0300

    Final

--- Assets/cubes/Controller.cs
+++ Assets/cubes/Controller.cs
@@ -4,62 +4,45 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour {
     [SerializeField] private GameObject[] container;
-    [SerializeField] private TextMesh[] text;
-	// Use this for initialization
-	void Start () {
-        for (int i = 0; i < container.Length; i++) { 
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < container.Length; i++)
+        {
             Debug.Log(container[i].name);
-            
-            
-        }      
+        }
     }
-	
-	// Update is called once per frame
-	void Update () {
-       
-    }
-   public void SelectSort() {
+    public void SelectSort() { 
         Debug.Log("\n");
         
-        for (int i = 0; i < container.Length-1; i++) {
+        for (int i = 0; i < container.Length; i++) {
             int min = i;
             for (int j = i + 1; j < container.Length; j++) {
-                if (System.Convert.ToInt32(container[j].name) < System.Convert.ToInt32(container[i].name))
+                Debug.Log(string.Format("{0} compare with {1}", container[j].name, container[min].name));
+                if (System.Convert.ToInt32(container[min].name) < System.Convert.ToInt32(container[j].name))
                 {
-                    Debug.Log(string.Format("{0} compare with {1}", container[j].name, container[min].name));
-                    Swap(container[j], container[i]);
-                    
+                    Debug.Log(string.Format("{0} swap with {1}", container[j].name, container[min].name));
+                    Swap(container[j], container[min]);
                 }
-                
+
             }
           
         }
+
         foreach (GameObject item in container) {
             Debug.Log(item.name);
         }
-        SwapT();
+       
 
     }
-    void SwapT() {
 
-        for (int i = 0; i < text.Length; i++) {
-            Debug.Log("T|C:::" + text[i].name + container[i].name);
-            text[i].name = container[i].name;
-            Debug.Log("T|C:::" + text[i].text + container[i].name);
-            text[i].text = container[i].name;
-        }
-        for (int i = 0; i < text.Length; i++) { Debug.Log("Контейнер:"+text[i].name + "/" + text[i].text); }
-        for (int i = 0; i < text.Length; i++) { Debug.Log(text[i].name + "/" + text[i].text); }
-        
-    }
     void Swap(GameObject a, GameObject b) {
         string name;
-       Vector3 position;
+       
         Material material;
 
-        name = a.name;
-        a.name = b.name;
-        b.name = name;
+
 
         material = a.GetComponent<MeshRenderer>().material;
         Debug.Log(material);
@@ -69,14 +52,40 @@ public class Controller : MonoBehaviour {
         b.GetComponent<MeshRenderer>().material = material;
         Debug.Log(b.GetComponent<MeshRenderer>().material);
 
-        position = a.transform.position;
-        a.transform.position = b.transform.position;
-        b.transform.position = position;
-        SwapT();
-    }
-    void Copy(GameObject a, GameObject b) {
+        Debug.Log("Names:" + " " + a.name + " " + b.name);
+            name = a.name;
+        Debug.Log("Names:" + " " + a.name + " " + b.name);
         a.name = b.name;
-        a.transform.position = b.transform.position;
-        a.GetComponent<MeshRenderer>().material = b.GetComponent<MeshRenderer>().material;
-    }
+        Debug.Log("Names:" + " " + a.name + " " + b.name);
+        b.name = name;
+        Debug.Log("Names:" + " " + a.name + " " + b.name);
+
+            //position = a.transform.position;
+           //a.transform.position = b.transform.position;
+           // b.transform.position = position;
+        // SwapT();
+            
+            Component AText = a.GetComponentInChildren<TextMesh>();
+            Component BText = b.GetComponentInChildren(typeof(TextMesh));
+            TextMesh A = AText as TextMesh;
+            TextMesh B = BText as TextMesh;
+
+            Debug.Log("Text:" + " " + A.text + " " + B.text);
+            string text = A.text;
+            Debug.Log("Text:" + " " + A.text + " " + B.text);
+            A.text = B.text;
+            Debug.Log("Text:" + " " + A.text + " " + B.text);
+            B.text = text;
+
+            Debug.Log("Text name:" + " " + A.text + " " + B.text);
+            string tname = A.name;
+            Debug.Log("Text name:" + " " + A.text + " " + B.text);
+            A.name = B.name;
+            Debug.Log("Text name:" + " " + A.text + " " + B.text);
+            B.name = tname;
+            
+
+
+        }
+
 }
