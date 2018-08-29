72646c8887c72d361ec04d5fbcf2dc02c27ea504
Cricetus <homyak1996@gmail.com>
2018-08-29 04:02:22 +0300

    Debugging

--- Assets/cubes/Controller.cs
+++ Assets/cubes/Controller.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour {
     [SerializeField] private GameObject[] container;
+    private int count;
 
     // Use this for initialization
     void Start()
@@ -13,37 +14,26 @@ public class Controller : MonoBehaviour {
             Debug.Log(container[i].name);
         }
     }
-    public void SelectSort() { 
-        Debug.Log("\n");
-        
-        for (int i = 0; i < container.Length; i++) {
-            int min = i;
-            for (int j = i + 1; j < container.Length; j++) {
-                Debug.Log(string.Format("{0} compare with {1}", container[j].name, container[min].name));
-                if (System.Convert.ToInt32(container[min].name) < System.Convert.ToInt32(container[j].name))
-                {
-                    Debug.Log(string.Format("{0} swap with {1}", container[j].name, container[min].name));
-                    Swap(container[j], container[min]);
-                }
-
+    public void SelectSort()
+    {
+        int min = 0 ;
+        int n = container.Length;
+        for (int i = 0; i < n - 1; ++i) {
+            min = i;
+            for (int j = i + 1; j < n; ++j)
+            {
+                if (System.Convert.ToInt32(container[j].name) < System.Convert.ToInt32(container[min].name)) { min = j;  }
+                 
             }
-          
+            if (min != i) { Swap(container[i], container[min]); }
         }
-
-        foreach (GameObject item in container) {
-            Debug.Log(item.name);
-        }
-       
-
     }
+ 
 
     void Swap(GameObject a, GameObject b) {
         string name;
-       
+
         Material material;
-
-
-
         material = a.GetComponent<MeshRenderer>().material;
         Debug.Log(material);
         Debug.Log(a.GetComponent<MeshRenderer>().material);
@@ -51,41 +41,41 @@ public class Controller : MonoBehaviour {
         Debug.Log(a.GetComponent<MeshRenderer>().material);
         b.GetComponent<MeshRenderer>().material = material;
         Debug.Log(b.GetComponent<MeshRenderer>().material);
-
+        
         Debug.Log("Names:" + " " + a.name + " " + b.name);
-            name = a.name;
+        name = a.name;
         Debug.Log("Names:" + " " + a.name + " " + b.name);
         a.name = b.name;
         Debug.Log("Names:" + " " + a.name + " " + b.name);
         b.name = name;
         Debug.Log("Names:" + " " + a.name + " " + b.name);
 
-            //position = a.transform.position;
-           //a.transform.position = b.transform.position;
-           // b.transform.position = position;
-        // SwapT();
-            
-            Component AText = a.GetComponentInChildren<TextMesh>();
-            Component BText = b.GetComponentInChildren(typeof(TextMesh));
-            TextMesh A = AText as TextMesh;
-            TextMesh B = BText as TextMesh;
+        GameObject g = a;
+        a = b;
+        b = g;
+       
 
-            Debug.Log("Text:" + " " + A.text + " " + B.text);
-            string text = A.text;
-            Debug.Log("Text:" + " " + A.text + " " + B.text);
-            A.text = B.text;
-            Debug.Log("Text:" + " " + A.text + " " + B.text);
-            B.text = text;
+        Component AText = a.GetComponentInChildren<TextMesh>();
+        Component BText = b.GetComponentInChildren(typeof(TextMesh));
+        TextMesh A = AText as TextMesh;
+        TextMesh B = BText as TextMesh;
 
-            Debug.Log("Text name:" + " " + A.text + " " + B.text);
-            string tname = A.name;
-            Debug.Log("Text name:" + " " + A.text + " " + B.text);
-            A.name = B.name;
-            Debug.Log("Text name:" + " " + A.text + " " + B.text);
-            B.name = tname;
-            
+        Debug.Log("Text:" + " " + A.text + " " + B.text);
+        string text = A.text;
+        Debug.Log("Text:" + " " + A.text + " " + B.text);
+        A.text = B.text;
+        Debug.Log("Text:" + " " + A.text + " " + B.text);
+        B.text = text;
+
+        Debug.Log("Text name:" + " " + A.text + " " + B.text);
+        string tname = A.name;
+        Debug.Log("Text name:" + " " + A.text + " " + B.text);
+        A.name = B.name;
+        Debug.Log("Text name:" + " " + A.text + " " + B.text);
+        B.name = tname;
+
+    }
 
 
-        }
 
 }
